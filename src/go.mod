5ec6dca2759a1bc947c46a9d98a9251c6c055a56
KuangjuX <18630816527@163.com>
2021-01-24 20:29:41 +0800

    且自逍遥没谁管

--- src/go.mod
+++ src/go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
--- src/routers/routers.go
+++ src/routers/routers.go
@@ -1,20 +1,40 @@
 package routers
 
 import(
-	"github.com/gin-gonic/gin"
+    "time"
+
+    "github.com/gin-gonic/gin"
+    "github.com/gin-contrib/cors"
     ."github.com/KuangjuX/Lang-Huan-Blessed-Land/Controllers"
     "github.com/KuangjuX/Lang-Huan-Blessed-Land/Middleware"
     "github.com/KuangjuX/Lang-Huan-Blessed-Land/Services/WebSocketService"
 )
 
 func InnitRouter() *gin.Engine{
-    routers := gin.Default()
+    routers := gin.New()
+    routers.Use(gin.Recovery())
+    routers.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"x-xq5-jwt", "Content-Type", "Origin", "Content-Length"},
+		ExposeHeaders:    []string{"x-xq5-jwt"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+    }))
+    
     root := routers.Group("/api")
     {   
         root.GET("/", Home)
         root.POST("register", Register)
         root.POST("loginByUsername", LoginByUsername)
         root.POST("loginByEmail", LoginByEmail)
+
+        //websocket test
+        hub := WebSocketService.NewHub()
+        go hub.Run()
+        root.GET("chat", func(c *gin.Context) { WebSocketService.ServeWs(hub, c) })
+
+       
         
         articles := root.Group("/articles")
         {
@@ -37,14 +57,19 @@ func InnitRouter() *gin.Engine{
             user.POST("modifyArticle", ModifyArticle)
             user.POST("likeArticle", LikeArticle)
             
-            userchat := user.Group("chat")
-            {
-                hub := WebSocketService.NewHub()
-                go hub.Run()
-                userchat.GET("/", func(c *gin.Context){WebSocketService.ServeWs(hub, c)})
-            }
+            // userchat := user.Group("chat")
+            // {
+            //     hub := WebSocketService.NewHub()
+            //     go hub.Run()
+            //     userchat.GET("/", func(c *gin.Context){WebSocketService.ServeWs(hub, c)})
+            // }
         }
     }
+
+    //websocket test
+    // hub := WebSocketService.NewHub()
+    // go hub.Run()
+    // routers.GET("ws", func(c *gin.Context) { WebSocketService.ServeWs(hub, c) })
 
     return routers;
 }
